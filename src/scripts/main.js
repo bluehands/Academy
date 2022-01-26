ef67d1d24fd46ea142ed1ff649e4c7872cb6b56f
Waldemar Tomme <waldetomme@gmail.com>
2022-01-26 15:58:37 +0000

    Implement crude search

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,36 @@
 const ko = require("knockout");
+const elasticlunr = require("elasticlunr");
 
 function createViewModel() {
     var vm = {
+        searchIndex: new elasticlunr.Index(),
         searchBarActive: ko.observable(false),
         searchBarInput: ko.observable("")
-            .extend({ rateLimit: 500 }),
+            .extend({ rateLimit: 250 }),
+        searchResults: ko.observable([]),
     };
 
     vm.searchBarInput.subscribe(search => {
         console.log(`Search: ${search}`)
+        const results = vm.searchIndex.search(search, {
+            bool: "OR",
+            expand: true
+        }).map((r) => vm.searchIndex.documentStore.getDoc(r.ref));
+        console.log(results);
+        vm.searchResults(results);
     });
 
     return vm;
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    ko.applyBindings(createViewModel());
+    var vm = createViewModel();
+
+    fetch("/search-index.json").then((response) => {
+        response.json().then((rawIndex) => {
+            vm.searchIndex = elasticlunr.Index.load(rawIndex);
+        });
+    })
+
+    ko.applyBindings(vm);
 });
